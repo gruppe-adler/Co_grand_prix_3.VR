5adbd9434be83b481b2c00547b70967658f5a9a1
Salbei <salbei@gruppe-adler.de>
2020-03-27 08:55:29 +0100

    added Sound for TP

--- USER/userSounds.hpp
+++ USER/userSounds.hpp
@@ -2,3 +2,10 @@
 *   Hier können eigene Sounds eingebunden werden.
 *   Ist in CfgSounds included.
 */
+
+class jumpTPSound
+{
+	name	= "jumpTPSound";
+	sound[]	= {"\data\jumpTPSound.ogg", db + 0, 1.0 };
+	titles[] = {};
+};
